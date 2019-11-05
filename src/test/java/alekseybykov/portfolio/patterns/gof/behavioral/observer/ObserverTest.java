85ae273d0a59cbf0722293cb0f795106fa8a739f
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-11-05 12:11:56 +0400

    Added additional test for example of usage Observer Pattern.

--- src/test/java/alekseybykov/portfolio/patterns/gof/behavioral/observer/ObserverTest.java
+++ src/test/java/alekseybykov/portfolio/patterns/gof/behavioral/observer/ObserverTest.java
@@ -3,10 +3,12 @@
 //
 package alekseybykov.portfolio.patterns.gof.behavioral.observer;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author  aleksey.n.bykov@gmail.com
@@ -15,13 +17,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Tests for Observer Pattern")
 class ObserverTest {
 
+    private Subject subject;
+    private FirstSubscriber firstSubscriber;
+    private SecondSubscriber secondSubscriber;
+
+    @BeforeEach
+    void init() {
+        subject = new Subject();
+        firstSubscriber = new FirstSubscriber();
+        secondSubscriber = new SecondSubscriber();
+    }
+
     @Test
     @DisplayName("Create a subscription and notify all the subscribers")
     void testCreateSubscribersAndNotifyAll() {
-        Subject subject = new Subject();
-
-        FirstSubscriber firstSubscriber = new FirstSubscriber();
-        SecondSubscriber secondSubscriber = new SecondSubscriber();
 
         subject.subscribe(firstSubscriber);
         subject.subscribe(secondSubscriber);
@@ -30,5 +39,27 @@ class ObserverTest {
 
         assertEquals("Announce", firstSubscriber.getAnnounces().get(0));
         assertEquals("Announce", secondSubscriber.getAnnounces().get(0));
+    }
+
+    @Test
+    @DisplayName("Unsubscribe one and notify all the subscribers")
+    void testUnsubscribeOneAndNotifyAll() {
+
+        subject.subscribe(firstSubscriber);
+        subject.subscribe(secondSubscriber);
+
+        subject.announce("Announce");
+
+        assertEquals("Announce", firstSubscriber.getAnnounces().get(0));
+        assertEquals("Announce", secondSubscriber.getAnnounces().get(0));
+
+        subject.unsubscribe(secondSubscriber);
+
+        subject.announce("Yet another announce");
+
+        assertTrue(secondSubscriber.getAnnounces().size() == 1);
+        assertEquals("Announce", secondSubscriber.getAnnounces().get(0));
+        assertEquals("Announce", firstSubscriber.getAnnounces().get(0));
+        assertEquals("Yet another announce", firstSubscriber.getAnnounces().get(1));
     }
 }
